2083f27b1aa67863c5e20170b6d5c97c847fb830
Xerun <xerun2000@gmail.com>
2022-05-08 23:10:16 +0200

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script>
+import "../app.css";
+import Navbar from "./navbar.svelte";
+</script>
+
+<Navbar/>
+
+<slot />
---
+++ src/routes/navbar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	const nav = [
+		{ title: 'Strona Główna', path: '/' },
+		{ title: 'Moja Lodówka', path: '/lodowka' },
+		{ title: 'Przepisy', path: '/przepisy' }
+	];
+</script>
+
+<nav>
+	{#each nav as item}
+		<a class:active={$page.url.pathname === item.path} href={item.path}>{item.title}</a>
+	{/each}
+</nav>
+
+<style lang="postcss">
+	nav {
+		@apply flex shadow-md py-4 mb-4 justify-center content-center;
+	}
+	a {
+		@apply flex flex-row p-4 mx-4 font-bold bg-white rounded-xl items-center text-center min-w-[48px] min-h-[48px];
+	}
+	a:hover {
+		@apply bg-blue-400 text-white transition-colors;
+	}
+	a.active {
+		@apply bg-blue-500 text-white;
+	}
+</style>
